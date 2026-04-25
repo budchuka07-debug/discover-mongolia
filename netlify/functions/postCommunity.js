45ef7fb3241f3c5dc0f21de96764a683eb297ad2
budchuka07-debug <budchuka07@gmail.com>
2026-04-25 11:36:40 -0700

    Update postCommunity.js

--- netlify/functions/postCommunity.js
+++ netlify/functions/postCommunity.js
@@ -1,1 +1,21 @@
 
+exports.handler = async (event) => {
+  try {
+    const data = JSON.parse(event.body);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Post received successfully",
+        data
+      })
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "Error processing request"
+      })
+    };
+  }
+};
